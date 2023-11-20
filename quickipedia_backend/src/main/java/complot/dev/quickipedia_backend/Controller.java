b71c59523eccea721de86face1ab3164fa30cdd9
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-20 14:22:46 +0100

    feat: added sanity checks tests

--- quickipedia_backend/src/main/java/complot/dev/quickipedia_backend/Controller.java
+++ quickipedia_backend/src/main/java/complot/dev/quickipedia_backend/Controller.java
@@ -11,5 +11,4 @@ public class Controller {
     public String helloTest(){
         return "Hello!";
     }
-
 }
--- quickipedia_backend/src/test/java/complot/dev/quickipedia_backend/ControllerTest.java
+++ quickipedia_backend/src/test/java/complot/dev/quickipedia_backend/ControllerTest.java
@@ -1,9 +1,38 @@
 package complot.dev.quickipedia_backend;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ControllerTest {
 
+    @Autowired
+    Controller controller;
 
+    @LocalServerPort
+    private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads(){
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    void helloEndpointShouldReturnHello(){
+        // Arrange
+        String response = "Hello!";
+        // Act
+        String url = this.restTemplate.getForObject("http://localhost:" + port + "/api/hello", String.class);
+        // Act & Assert
+        assertThat(url).contains(response);
+    }
 }
