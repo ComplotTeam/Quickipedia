8673d78036437696bb77e5d850a51c30e97dd5b2
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-20 11:06:25 +0100

    feat: backend folder created

---
+++ quickipedia_backend/src/main/java/complot/dev/quickipedia_backend/QuickipediaBackendApplication.java
@@ -0,0 +1,13 @@
+package complot.dev.quickipedia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickipediaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickipediaBackendApplication.class, args);
+	}
+
+}
---
+++ quickipedia_backend/src/test/java/complot/dev/quickipedia_backend/QuickipediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package complot.dev.quickipedia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickipediaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
