066043253d8adaff07a3ed3dd899ddb0bfbb082c
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-20 20:37:09 +0100

    feat: Connected frontend to backend

--- quickipedia_frontend/app/page.tsx
+++ quickipedia_frontend/app/page.tsx
@@ -1,8 +1,22 @@
+"use client"
+import axios from "axios"
+import { useEffect } from "react";
+
 
 export default function Home() {
+
+  const fetchHello = async () => {
+    const response = await axios.get("http://localhost:8080/api/hello");
+    const hello = response.data;
+    console.log(hello);
+  }
+  useEffect(() => {
+    fetchHello();
+  },[])
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      {}
     </main>
   )
 }
