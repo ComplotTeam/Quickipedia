27351343fc4a92e65e6ef5435242926a6fdb6ede
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-21 19:34:22 +0100

    feat: created Navbar component with predefined links

--- quickipedia_frontend/app/components/navbar/Navbar.tsx
+++ quickipedia_frontend/app/components/navbar/Navbar.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+
+  <nav className="bg-slate-100 border-gray-200">
+    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-900 hover:text-gray-400">Quickipedia</span>
+        </Link>
+        <div className="flex items-center space-x-6 rtl:space-x-reverse">
+            <Link href="/about" className="text-sm  text-gray-900 hover:underline">About us</Link>
+        </div>
+    </div>
+    <div className="flex flex-row justify-around items-center max-w-screen-xl px-4 py-3 mx-auto">
+            <button className="flex flex-row justify-center items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+
+                    <Link href="/trending" className="text-gray-900 hover:underline" aria-current="page">TRENDING</Link>
+                    </button>
+                    <button className="flex flex-row justify-center items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                    <Link href="/allArticles" className="text-gray-900 hover:underline">FIND ALL</Link>
+                    </button>
+        </div>
+  </nav>
+
   )
 }
