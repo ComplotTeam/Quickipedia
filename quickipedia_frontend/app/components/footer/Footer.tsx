14cce80d56941e0413104be53a46e9c264f755a1
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-21 20:05:17 +0100

    feat: added footer with appropriate links

--- quickipedia_frontend/app/components/footer/Footer.tsx
+++ quickipedia_frontend/app/components/footer/Footer.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-slate-100 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6  text-gray-900">
+      <div className="sm:flex sm:items-center sm:justify-between w-full">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium">
+            <li>
+              <Link href="/" className="hover:underline me-4 md:me-6">
+                Home
+              </Link>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Search
+              </a>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+
+        <span className="text-sm sm:text-center hover:underline">
+          © 2023{" "}
+          <Link
+            href="https://github.com/ComplotTeam/Quickipedia"
+            className="hover:underline"
+          >
+            Quickipedia™
+          </Link>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
--- quickipedia_frontend/app/components/navbar/Navbar.tsx
+++ quickipedia_frontend/app/components/navbar/Navbar.tsx
@@ -1,28 +1,43 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 export const Navbar = () => {
   return (
-
-  <nav className="bg-slate-100 border-gray-200">
-    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-900 hover:text-gray-400">Quickipedia</span>
+    <nav className="fixed top-0 w-full bg-slate-100 border-gray-200">
+      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <Link
+          href="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-900 hover:text-gray-400">
+            Quickipedia
+          </span>
         </Link>
         <div className="flex items-center space-x-6 rtl:space-x-reverse">
-            <Link href="/about" className="text-sm  text-gray-900 hover:underline">About us</Link>
+          <Link
+            href="/about"
+            className="text-sm  text-gray-900 hover:underline"
+          >
+            About us
+          </Link>
         </div>
-    </div>
-    <div className="flex flex-row justify-around items-center max-w-screen-xl px-4 py-3 mx-auto">
-            <button className="flex flex-row justify-center items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
-
-                    <Link href="/trending" className="text-gray-900 hover:underline" aria-current="page">TRENDING</Link>
-                    </button>
-                    <button className="flex flex-row justify-center items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
-                    <Link href="/allArticles" className="text-gray-900 hover:underline">FIND ALL</Link>
-                    </button>
-        </div>
-  </nav>
-
-  )
-}
+      </div>
+      <div className="flex flex-row justify-around items-center max-w-screen-xl px-4 py-3 mx-auto">
+        <button className="flex flex-row justify-center items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+          <Link
+            href="/trending"
+            className="text-gray-900 hover:underline"
+            aria-current="page"
+          >
+            TRENDING
+          </Link>
+        </button>
+        <button className="flex flex-row justify-center items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+          <Link href="/allArticles" className="text-gray-900 hover:underline">
+            FIND ALL
+          </Link>
+        </button>
+      </div>
+    </nav>
+  );
+};
