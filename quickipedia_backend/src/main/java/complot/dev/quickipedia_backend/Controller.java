97624f5d064abdbb69c460a9dea1b7d653d861bb
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-21 18:35:48 +0100

    feat: Added endpoint for getTopArticles

--- quickipedia_backend/src/main/java/complot/dev/quickipedia_backend/Controller.java
+++ quickipedia_backend/src/main/java/complot/dev/quickipedia_backend/Controller.java
@@ -4,7 +4,9 @@ import complot.dev.quickipedia_backend.dtos.ArticleResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -16,25 +18,82 @@ public class Controller {
         return "Hello!";
     }
 
-    @GetMapping("/getAllArticles")
+    @GetMapping("/articles")
     public ResponseEntity<List<ArticleResponseDto>> getAllArticles() {
 
-        return ResponseEntity.ok().body(
-                List.of(
-                        new ArticleResponseDto(
-                                "Is the sky blue?",
-                                "Yes it certainly is",
-                                "Controversial",
-                                1
-                        ),
-                        new ArticleResponseDto(
-                                "Is Colin the best coder in the world?",
-                                "Yes he certainly is",
-                                "Facts",
-                                2
-                        )
-                )
+        return ResponseEntity.ok().body(tempList());
+    }
 
+    @GetMapping("/articles/top")
+    public ResponseEntity<List<ArticleResponseDto>> getTopArticles() {
+
+        List<ArticleResponseDto> sortedArticles = tempList().stream()
+                .sorted(Comparator.comparing(ArticleResponseDto::rank))
+                .limit(5)
+                .toList();
+        return ResponseEntity.ok(sortedArticles);
+    }
+
+    //To be removed
+    private List<ArticleResponseDto> tempList(){
+        return List.of(
+                new ArticleResponseDto(
+                        "Is the sky blue?",
+                        "Yes it certainly is",
+                        "Controversial",
+                        3
+                ),
+                new ArticleResponseDto(
+                        "Is Colin the best coder in the world?",
+                        "Yes he certainly is",
+                        "Facts",
+                        2
+                ),
+                new ArticleResponseDto(
+                        "Is Marisa the best coder in the world?",
+                        "Yes she certainly is",
+                        "Facts",
+                        1
+                ),
+                new ArticleResponseDto(
+                        "What year is it?",
+                        "2023",
+                        "News",
+                        7
+                ),
+
+                new ArticleResponseDto(
+                        "What happens tomorrow?",
+                        "We don't know but it's wednesday",
+                        "News",
+                        4
+                ),
+
+                new ArticleResponseDto(
+                        "Where is sweden?",
+                        "Northern europe, scandinavia",
+                        "Geography",
+                        8
+                ),
+
+                new ArticleResponseDto(
+                        "What year is it next year?",
+                        "2024",
+                        "News",
+                        9
+                ),
+                new ArticleResponseDto(
+                        "How do you cook?",
+                        "You mix vegetables and spices",
+                        "Food",
+                        6
+                ),
+                new ArticleResponseDto(
+                        "Where is portugal?",
+                        "Next to spain",
+                        "Geography",
+                        5
+                )
         );
     }
 }
