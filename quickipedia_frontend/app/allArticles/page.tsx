d9a8a107ba0a4bac9506be02be895308b69d7fd0
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-21 21:23:53 +0100

    feat: added all articles page functionality

--- quickipedia_frontend/app/allArticles/page.tsx
+++ quickipedia_frontend/app/allArticles/page.tsx
@@ -1,9 +1,29 @@
 "use client"
-import React from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react";
+import { Article } from "../page";
+import { ArticleTitle } from "../components";
 
 const Page = () => {
+  const [allArticles, setAllArticles] = useState<Article[]>();
+  const fetchArticles = async () => {
+    const response = await axios.get("https://quickipedia.azurewebsites.net/api/articles");
+    const data: Article[] = response.data;
+    setAllArticles(data);
+  }
+  useEffect(() => {
+    fetchArticles();
+  },[])
   return (
-    <div>page</div>
+    <main className="flex min-h-screen mt-10 flex-col items-center justify-between p-24">
+      <ol>
+      {allArticles && allArticles.map((article, index) => (
+        <li key={index}>
+            <ArticleTitle {...article} />
+        </li>
+         ))}
+      </ol>
+    </main>
   )
 }
 
