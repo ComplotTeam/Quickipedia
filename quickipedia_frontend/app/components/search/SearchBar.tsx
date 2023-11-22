9f950cb30ef6d0958d4875d10a8c25e4835cb4b2
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-22 08:20:07 +0100

    refactor: handling fetching on search page

--- quickipedia_frontend/app/components/search/SearchBar.tsx
+++ quickipedia_frontend/app/components/search/SearchBar.tsx
@@ -1,23 +1,6 @@
-"use client"
-import { Article } from "@/app/page";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export const SearchBar = () => {
-  const [articles, setArticles] = useState<Article[]>();
-  const [searchedArticle, setSearchedArticle] = useState<Article>();
-
-  const fetchArticles = async () => {
-    const response = await axios.get(
-      "https://quickipedia.azurewebsites.net/api/articles"
-    );
-    const data: Article[] = response.data;
-    setArticles(data);
-  };
-
-  useEffect(() => {
-    fetchArticles();
-  }, []);
 
   return <div>SearchBar</div>;
 };
--- quickipedia_frontend/app/search/page.tsx
+++ quickipedia_frontend/app/search/page.tsx
@@ -1,12 +1,30 @@
 "use client"
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Article } from '../page';
+import { SearchBar } from '../components';
 
-import React from 'react'
-import { SearchBar } from '../components'
-
-export const Search = () => {
+const Page = () => {
+    const [articles, setArticles] = useState<Article[]>();
+    const [searchedArticle, setSearchedArticle] = useState<Article>();
+  
+    const fetchArticles = async () => {
+      const response = await axios.get(
+        "https://quickipedia.azurewebsites.net/api/articles"
+      );
+      const data: Article[] = response.data;
+      setArticles(data);
+    };
+  
+    useEffect(() => {
+      fetchArticles();
+    }, []);
+  
   return (
     <main className="flex min-h-screen mt-10 flex-col items-center justify-between p-24">
         <SearchBar/>
         </main>
   )
 }
+
+export default Page;
