32b53dac1de12590f8eff8a3908e934d41a31b61
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-22 11:58:09 +0100

    feat: Adds custom colors scheme for frontend

--- quickipedia_frontend/app/components/navbar/Navbar.tsx
+++ quickipedia_frontend/app/components/navbar/Navbar.tsx
@@ -5,20 +5,20 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full bg-slate-100 border-gray-200">
+    <nav className="fixed top-0 w-full bg-primary border-gray-200">
       <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <Link
           href="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-900 hover:text-gray-400">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-primaryB hover:text-gray-400">
             <h1 className="">Quickipedia</h1>
           </span>
         </Link>
         <div className="flex items-center space-x-6 rtl:space-x-reverse">
           <Link
             href="/about"
-            className="text-sm  text-gray-900 hover:underline"
+            className="text-sm  text-primaryB hover:underline"
           >
             About us
           </Link>
@@ -28,14 +28,14 @@ export const Navbar = () => {
         <button className="flex flex-row justify-center items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
           <Link
             href="/"
-            className="text-gray-900 hover:underline"
+            className="text-primaryB hover:underline"
             aria-current="page"
           >
             TRENDING
           </Link>
         </button>
         <button className="flex flex-row justify-center items-center font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
-          <Link href="/allArticles" className="text-gray-900 hover:underline">
+          <Link href="/allArticles" className=" text-primaryB hover:underline">
             FIND ALL
           </Link>
         </button>
--- quickipedia_frontend/app/layout.tsx
+++ quickipedia_frontend/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className=" bg-primaryB">
       <body className={ubuntu.className}>
         <Navbar/>
         {children}
