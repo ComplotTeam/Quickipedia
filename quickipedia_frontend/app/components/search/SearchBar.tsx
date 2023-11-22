7f7e7b2d323949487eb6de96f13e47fe420e7146
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-22 08:14:55 +0100

    feat: skeleton of search bar

--- quickipedia_frontend/app/components/search/SearchBar.tsx
+++ quickipedia_frontend/app/components/search/SearchBar.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+"use client"
+import { Article } from "@/app/page";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
+  const [articles, setArticles] = useState<Article[]>();
+  const [searchedArticle, setSearchedArticle] = useState<Article>();
+
+  const fetchArticles = async () => {
+    const response = await axios.get(
+      "https://quickipedia.azurewebsites.net/api/articles"
+    );
+    const data: Article[] = response.data;
+    setArticles(data);
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  return <div>SearchBar</div>;
+};
