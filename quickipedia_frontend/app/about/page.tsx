d47ce166d0c3f226f8c798325d9e92ac1eff082b
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-22 10:26:05 +0100

    feat: skeleton of about us page

--- quickipedia_frontend/app/about/page.tsx
+++ quickipedia_frontend/app/about/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 
 export default function About() {
     return <main className="flex min-h-screen mt-10 flex-col items-center justify-between p-24">
-        <h1>About us</h1>
-        <p></p>
+        <h1>About Quickipedia</h1>
+        <p>Quickipedia is an ambitious project aimed at creating a real-time online platform that aggregates and displays trending information across diverse categories like nature, cars, culture, healthcare, and cats. Our vision is to develop an intuitive, user-friendly interface backed by a robust, scalable architecture that delivers up-to-the-minute trending content to our users.</p>
+        <br/>
+        <h2>Discover the Complot Team!</h2>
+        <Link href="https://www.linkedin.com/in/colinnordmark/" className="hover:underline">Colin Nordmark</Link><Link href="https://github.com/colinnordmark" className="hover:underline"> (GitHub)</Link>
+        <Link href="https://www.linkedin.com/in/marisa-pinheiro-833a12113/" className="hover:underline">Marisa Pinheiro</Link><Link href="https://github.com/Marisa-Pinheiro" className="hover:underline"> (GitHub)</Link>
     </main>
 } 
