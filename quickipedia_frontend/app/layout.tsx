862f21dc264ee14dc5a338d65c6af5b525e2bac4
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-22 11:01:12 +0100

    refactor: removing export from font ubuntu mono

--- quickipedia_frontend/app/layout.tsx
+++ quickipedia_frontend/app/layout.tsx
@@ -1,30 +1,36 @@
-import type { Metadata } from 'next'
-import { Ubuntu ,Ubuntu_Mono} from 'next/font/google'
-import './globals.css'
-import { Footer, Navbar } from './components'
+import type { Metadata } from "next";
+import { Ubuntu, Ubuntu_Mono } from "next/font/google";
+import "./globals.css";
+import { Footer, Navbar } from "./components";
 
-const ubuntu = Ubuntu({ 
-  weight: ["300","400","500","700"],
-  subsets: ['latin'] })
+const ubuntu = Ubuntu({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+});
 
-export const ubuntuMono = Ubuntu_Mono({
-  weight: ["400","700"],
-  subsets: ['latin']})
-
+const ubuntuMono = Ubuntu_Mono({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: '--font-ubuntu-mono'
+});
 
 export const metadata: Metadata = {
-  title: 'Quickipedia',
-  description: 'Created by complot team',
-}
+  title: "Quickipedia",
+  description: "Created by complot team",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={ubuntu.className}><Navbar/>{children}<Footer/></body>
+      <body className={ubuntu.className}>
+        <Navbar/>
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
