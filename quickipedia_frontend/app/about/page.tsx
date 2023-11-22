1f763c024a9a9e9996d84fc496418edbd073dbca
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-22 08:07:29 +0100

    feat: added structure for about us page

--- quickipedia_frontend/app/about/page.tsx
+++ quickipedia_frontend/app/about/page.tsx
@@ -1,4 +1,7 @@
 
 export default function About() {
-    return <div>About</div>
+    return <main className="flex min-h-screen mt-10 flex-col items-center justify-between p-24">
+        <h1>About us</h1>
+        <p></p>
+    </main>
 } 
