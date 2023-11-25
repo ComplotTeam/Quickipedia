6def21d33ea61f6f3ddd4246818f22a7ebe8d3fa
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-25 21:04:58 +0100

    style: Color adjustment to filter buttons

--- quickipedia_frontend/app/components/filter-button/Filterbutton.tsx
+++ quickipedia_frontend/app/components/filter-button/Filterbutton.tsx
@@ -12,7 +12,7 @@ export const Filterbutton = ({topic}: Props) => {
   return (
     <>
     <button type="button" 
-    className="flex flex-row flex-wrap focus:outline-none text-white bg-secondary hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-small rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">
+    className="flex flex-row flex-wrap focus:outline-none text-white bg-rose-700 hover:bg-rose-500 focus:ring-4 focus:ring-rose-300 font-small rounded-xl text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-rose-900">
         <a href={url}>
     {topic.toUpperCase()}
     </a>
