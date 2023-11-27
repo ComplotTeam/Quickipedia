a5a31ca48129546b5ea5a9fec7c3cd004bb3a923
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-27 13:06:06 +0100

    feat: Added buttons to articles for beckend implementation

--- quickipedia_frontend/app/components/index.ts
+++ quickipedia_frontend/app/components/index.ts
@@ -6,7 +6,7 @@ import { LoginButton } from "./login-button/LoginButton";
 import { LogoutButton } from "./logout-button/LogoutButton";
 import { Filterbutton } from "./filter-button/Filterbutton";
 import { NavFooter } from "./nav-footer/NavFooter";
-
+import { BookmarkButton } from "./bookmark-button/BookmarkButton";
 export {
   Navbar,
   Footer,
@@ -16,4 +16,5 @@ export {
   LogoutButton,
   Filterbutton,
   NavFooter,
+  BookmarkButton
 };
