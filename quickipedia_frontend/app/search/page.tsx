a8ef9d70bdb5ea5b29a0a202c941addf0fada335
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-28 12:52:22 +0100

    refactor: extracting blocks of repeated logic from search page

--- quickipedia_frontend/app/search/page.tsx
+++ quickipedia_frontend/app/search/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import React, { ChangeEvent, useEffect, useState } from "react";
-import axios from "axios";
 import { Article } from "@/app/utils/types";
 import { ArticleTitle, NavFooter } from "../components";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { UserData } from "@/app/utils/types";
+import { handleBookmarking } from "../utils/bookmarksUtils";
+import { fetchArticles } from "../utils/articlesUtils";
+import { fetchUserBookmarks } from "../utils/userBookmarksUtils";
 
 const Page = () => {
   const { user, isLoading } = useUser();
   const [articles, setArticles] = useState<Article[]>();
   const [filteredArticles, setFilteredArticles] = useState<Article[]>();
-  const [searchedArticle, setSearchedArticle] = useState<string>();
+  const [searchedArticle, setSearchedArticle] = useState<string>("");
   const [userBookmarks, setUserBookmarks] = useState<Article[]>();
 
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
@@ -25,59 +26,35 @@ const Page = () => {
     setFilteredArticles(filteredArticles);
   };
 
-  const fetchArticles = async () => {
-    const response = await axios.get(
-      "https://quickipedia.azurewebsites.net/api/articles"
-    );
-    const data: Article[] = response.data;
-    setArticles(data);
-    setFilteredArticles(data);
-  };
-
-  const fetchUserBookmarks = async () => {
-    const response = await axios({
-      method: "get",
-      url: `https://quickipedia.azurewebsites.net/api/users/${user?.email}`,
-    });
-    const userData: UserData = response.data;
-    const bookmarks = userData.bookmarks;
-
-    setUserBookmarks(bookmarks);
-  };
-
-  const handleBookmarking = async (email: string, articleToToggle: Article) => {
-    if (!userBookmarks) {
-      return;
-    }
-    if (
-      userBookmarks?.filter((item) => item.id == articleToToggle.id).length > 0
-    ) {
-      //console.log("you tried to remove a bookmark");
-      await axios({
-        method: "delete",
-        url: `https://quickipedia.azurewebsites.net/api/users/${email}`,
-        data: { id: articleToToggle.id },
-      });
-      setUserBookmarks(
-        userBookmarks?.filter((item) => item.id != articleToToggle.id)
-      );
-      return;
-    }
-    const data = await axios({
-      method: "post",
-      url: `https://quickipedia.azurewebsites.net/api/users/${email}`,
-      data: { id: articleToToggle.id },
-    });
-    const articles: Article[] = [...userBookmarks, articleToToggle];
-    setUserBookmarks(articles);
-  };
-
   useEffect(() => {
-    fetchArticles();
-  }, []);
+    const fetchData = async () => {
+      if (!user?.email) {
+        console.error("User email is undefined");
+        return;
+      }
+      try {
+        const userData = await fetchUserBookmarks(user.email);
+        setUserBookmarks(userData.bookmarks);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
 
-  if (isLoading && !userBookmarks) return <div>Loading...</div>;
-  if (!isLoading && !userBookmarks) fetchUserBookmarks();
+    const fetchAllArticles = async () => {
+      try {
+        const data = await fetchArticles();
+        setArticles(data);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      }
+    };
+
+    if (!isLoading && !userBookmarks) {
+      fetchData();
+    }
+
+    fetchAllArticles();
+  }, [user?.email, userBookmarks, isLoading]);
 
   return (
     <main className="flex min-h-screen mt-10 flex-col items-center justify-start py-24">
@@ -101,7 +78,12 @@ const Page = () => {
                 {...article}
                 bookmarks={userBookmarks || []}
                 toggleBookmark={() =>
-                  handleBookmarking(user?.email || "", article)
+                  handleBookmarking(
+                    user?.email || "",
+                    article,
+                    userBookmarks,
+                    setUserBookmarks
+                  )
                 }
               />
             ))
