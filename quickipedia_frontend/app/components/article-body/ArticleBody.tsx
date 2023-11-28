775727e7534a3abd2dd83ec0adfd6805d083912c
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-28 15:16:11 +0100

    feat: Added link and voice to article body

--- quickipedia_frontend/app/components/article-body/ArticleBody.tsx
+++ quickipedia_frontend/app/components/article-body/ArticleBody.tsx
@@ -2,13 +2,16 @@
 import React from "react";
 import { Article } from "@/app/utils/types";
 import Link from "next/link";
+import { SpeechButton } from "..";
 
 export const ArticleBody = (props: Article) => {
-  console.log(props);
   return (
     <>
       <div className=" bg-slate-800 border border-primaryB px-6 text-slate-100 py-3 rounded-xl  mb-3 ">
-        <h1 className="pb-2 font-medium">Answer:</h1>
+        <div className="pb-2 flex flex-row align-baseline justify-between">
+          <h1 className="font-medium">Answer:</h1>
+          <SpeechButton text={props.answer}/>
+        </div>
         <p
           data-te-modal-init
           id="default-modal"
