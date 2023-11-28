f12b457c154b324098830cafc84f3ccd629ff97e
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-28 12:38:39 +0100

    refactor: extracting logic from all articles page

---
+++ quickipedia_frontend/app/utils/articlesUtils.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { Article } from "@/app/utils/types";
+
+export const fetchArticles = async (): Promise<Article[]> => {
+  try {
+    const response = await axios.get(
+      "https://quickipedia.azurewebsites.net/api/articles"
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+    throw error;  //look into how we want to do this 
+  }
+};
