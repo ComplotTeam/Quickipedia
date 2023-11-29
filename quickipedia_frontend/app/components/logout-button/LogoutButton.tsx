1f73c07057613a945c44124ffd11f9d910d87f4b
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-29 19:00:08 +0100

    fix: Additional hover fixes

--- quickipedia_frontend/app/components/logout-button/LogoutButton.tsx
+++ quickipedia_frontend/app/components/logout-button/LogoutButton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const LogoutButton = () => {
   return (
     <>
-      <button className=" text-secondaryB border border-secondaryB rounded-xl hover:underline hover:bg-secondaryB hover:text-primaryB mx-auto px-3 py-1">
+      <button className=" text-secondaryB border border-secondaryB rounded-xl betterhover:hover:bg-secondaryB betterhover:hover:text-primaryB mx-auto px-3 py-1">
         <a href="/api/auth/logout">LOGOUT</a>
       </button>
     </>
