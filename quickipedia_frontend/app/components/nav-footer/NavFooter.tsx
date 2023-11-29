58a98f3ad4a89e6cbd6b154b70cc79c60e520b00
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-29 11:49:42 +0100

    refactor: centered Q in navfooter

--- quickipedia_frontend/app/components/nav-footer/NavFooter.tsx
+++ quickipedia_frontend/app/components/nav-footer/NavFooter.tsx
@@ -27,7 +27,7 @@ export const NavFooter = () => {
           role="tooltip"
           className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
         >
-          Home
+          Trending
           <div className="tooltip-arrow" data-popper-arrow></div>
         </div>
 
@@ -42,9 +42,9 @@ export const NavFooter = () => {
         <div
           id="tooltip-new"
           role="tooltip"
-          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          className="absolute z-10 invisible inline-block px-3 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
         >
-          Create new item
+          All
           <div className="tooltip-arrow" data-popper-arrow></div>
         </div>
         <Link
@@ -75,7 +75,7 @@ export const NavFooter = () => {
           role="tooltip"
           className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
         >
-          Settings
+          Search
           <div className="tooltip-arrow" data-popper-arrow></div>
         </div>
       </div>
