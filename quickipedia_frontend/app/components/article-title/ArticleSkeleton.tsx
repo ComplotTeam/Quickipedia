6225811574c1e89e42b6faa43344c587ea7bbbf2
colinnordmark <nordmarkcolin@gmail.com>
2023-12-18 20:27:05 +0100

    chore: Adds semantic html to text components in skeleton

--- quickipedia_frontend/app/components/article-title/ArticleSkeleton.tsx
+++ quickipedia_frontend/app/components/article-title/ArticleSkeleton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ArticleSkeleton = () => {
   return (
-    <div
+    <article
       className="p-4 flex flex-col w-[100%] rounded-xl mt-3 mb-3 bg-white text-slate-950 text-lg drop-shadow-xl [...]
       relative 
       before:absolute before:inset-0
@@ -11,24 +11,24 @@ export const ArticleSkeleton = () => {
       before:bg-gradient-to-r
       before:from-transparent before:via-primary/25 before:to-transparent
       isolate
-    overflow-hidden
-    shadow-xl 
+      overflow-hidden
+      shadow-xl 
     shadow-black/5
-    before:border-t 
+      before:border-t 
     before:border-rose-100/10"
     >
       <div className=" animate-pulse flex flex-col space-y-4">
         <div className="flex justify-between">
-          <div className="h-6 bg-gray-300 rounded w-3/4"></div>
+          <h1 className="h-6 bg-gray-300 rounded w-3/4"></h1>
           <div className="h-6 bg-gray-300 rounded w-8"></div>
         </div>
-        <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+        <h2 className="h-4 bg-gray-200 rounded w-5/6"></h2>
         <div className="flex justify-end space-x-2">
-          <div className="h-8 bg-gray-300 rounded w-12"></div>
-          <div className="h-8 bg-gray-300 rounded w-8"></div>
+          <div className="h-8 bg-gray-200 rounded w-12"></div>
+          <div className="h-8 bg-gray-200 rounded w-8"></div>
         </div>
       </div>
-    </div>
+    </article>
   );
 };
 
