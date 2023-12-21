61af698f51fa8bf3794f6b958fc99966dd11404c
colinnordmark <nordmarkcolin@gmail.com>
2023-12-21 15:53:07 +0100

    style: corrected sizing for topic component

--- quickipedia_frontend/app/components/topic-button/TopicButton.tsx
+++ quickipedia_frontend/app/components/topic-button/TopicButton.tsx
@@ -6,21 +6,22 @@ type Props = {
 };
 
 export const TopicButton = ({ topicOne, topicTwo }: Props) => {
-  let urlOne: string = "/articles/" + topicOne;
-  let urlTwo: string = "/articles/" + topicOne;
+  let urlOne: string = "/articles/" + topicOne.toLowerCase();
+  let urlTwo: string = "/articles/" + topicTwo.toLowerCase();
+  
 
   const bg =
-    "bg-gradient-to-br from-secondaryB to-secondaryB-300  betterhover:hover:bg-gradient-to-tl betterhover:hover:from-ternary-500 betterhover:hover:to-ternary-200 ";
+    "bg-gradient-to-br from-secondaryB to-secondaryB-300  betterhover:hover:bg-gradient-to-tl betterhover:hover:from-ternary-500 betterhover:hover:to-ternary-200 drop-shadow-sm ";
   return (
-    <article className="group/article p-4 text-left flex flex-row justify-between w-[100%] text-grey-900 rounded-xl mt-3 mb-3 border border-primaryB text-primaryB text-lg drop-shadow-xl sm:ml-0 space-x-4">
+    <article className="group/article p-3 bg-primaryB/80 text-left flex flex-row justify-between w-[100%] text-grey-900 rounded-xl mt-3 mb-3 text-primaryB text-lg drop-shadow-xl sm:ml-0 space-x-2 h-28">
       <Link
-        className={bg + "text-left font-medium rounded-lg text-lg px-5 py-3 mr-2 w-full"}
+        className={bg + "text-left font-medium rounded-lg text-lg px-5 py-3 mr-2 w-full transition ease-in-out duration-300"}
         href={urlOne}
       >
         {topicOne}
       </Link>
       <Link
-        className={bg + "text-left font-medium rounded-lg text-lg px-5 py-3 mr-2 w-full"}
+        className={bg + "text-left font-medium rounded-lg text-lg px-5 py-3 mr-2 w-full transition ease-in-out duration-300"}
         href={urlTwo}
       >
         {topicTwo}
