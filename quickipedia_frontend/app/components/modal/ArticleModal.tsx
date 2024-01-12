8e9121b632cb5c56bfe741ba088e973dd4d1cd60
colinnordmark <nordmarkcolin@gmail.com>
2024-01-12 21:09:46 +0100

    style: Fixes so that modal has more appropriate sizing for different displays

--- quickipedia_frontend/app/components/modal/ArticleModal.tsx
+++ quickipedia_frontend/app/components/modal/ArticleModal.tsx
@@ -10,17 +10,25 @@ interface ArticleModalProps extends ArticleBody {
   isVisible: boolean;
 }
 
-export const ArticleModal = ({ article, isVisible, onClose }: ArticleModalProps) => {
+export const ArticleModal = ({
+  article,
+  isVisible,
+  onClose,
+}: ArticleModalProps) => {
   const handleCloseClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     onClose();
   };
 
-  const modalClass = isVisible ? 'animate-expand lg:animate-expand_high' : 'animate-shrink lg:animate-shrink_high';
+  const modalClass = isVisible
+    ? "animate-expand lg:animate-expand_high"
+    : "animate-shrink lg:animate-shrink_high";
   console.log("Modal class: " + modalClass);
   const modalContent = (
-    <>
-      <div className={`border border-gray-200 bg-primaryB px-6 text-left text-secondary py-3 rounded-xl mb-3 ${modalClass} overflow-hidden fixed top-20 left-0 right-0 mx-5 drop-shadow-lg`}>
+    <div
+      className={`flex justify-around ${modalClass} overflow-hidden fixed top-20 left-0 right-0 mx-5 drop-shadow-lg`}
+    >
+      <div className="border border-gray-200 bg-primaryB rounded-xl mb-3 px-6 text-left text-secondary py-3 w-[85%] md:w-[90%] lg:w-[70%]">
         <div className="pb-2 flex justify-between">
           {<SpeechButton text={article.answer} />}
           {<button onClick={onClose}> close </button>}
@@ -44,7 +52,7 @@ export const ArticleModal = ({ article, isVisible, onClose }: ArticleModalProps)
           {"Read more at " + article.source.split(".")[1]}
         </Link>
       </div>
-    </>
+    </div>
   );
   const modalRoot = document.getElementById("modal-root");
   if (modalRoot) {
